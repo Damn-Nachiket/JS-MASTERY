bbd477c71925bffb0c77329b05e7b299852b3c70
Nachiket Patel <142562120+Damn-Nachiket@users.noreply.github.com>
2024-08-01 17:57:10 +0000

    understood the js all variables

---
+++ BASIC_JS/VAR.js
@@ -0,0 +1,17 @@
+const signinId = 989288
+let signEMailId = "world@google.com"
+var signPassword = "09876453"
+signUsername = "WALTER WHITE"
+let signLocation 
+
+console.log("EXECUTION")
+console.table([signinId, signEMailId, signPassword, signUsername, signLocation])
+
+// --------------NOTES
+
+// console.log(signinId) 
+// console.table([signinId, signEMailId, signPassword, signUsername, signLocation])
+
+// const = const is fix value yoy can't change it after giving valaue to it.
+// var = problem in past in block scope and functional scope , not preferable to use 
+// let = solution of the var problem . currently only const and let use in JS projects. 
