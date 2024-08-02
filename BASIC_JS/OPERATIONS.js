c6481f062640c8c7518d164bf744ac4d8b6022f0
Nachiket Patel <142562120+Damn-Nachiket@users.noreply.github.com>
2024-08-02 04:36:05 +0000

    Operations sections added

---
+++ BASIC_JS/OPERATIONS.js
@@ -0,0 +1,29 @@
+let value = 2
+let negVal = -value
+console.log(value)
+console.log(negVal)
+
+let str1 = "HELLO"
+let str2 = " NACHIKET"
+let str3 = "WELCOME AND " + str1 + str2 
+console.log(str3)
+
+console.log(1 + "2")
+console.log("1" + 2)
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(+true)
+console.log(+"")
+
+let num1, num2 , num3
+num1 = num2 = num3 = 20 - 10
+
+// DO SOME OPERATIONS OF PREFIX AND POSTFIX FROM ECMA OR MDN
+
+let a = 20
+console.log(a)
+let aa = ++a
+console.log(aa)
+let aaa = a++
+console.log(aaa)
