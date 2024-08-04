37848b5c9691792d75cce4e1d8979c7bc23a8557
Nachiket Patel <142562120+Damn-Nachiket@users.noreply.github.com>
2024-08-04 15:21:03 +0000

    OBJECTS noted some more coming

---
+++ BASIC2_JS/OBJECTS.js
@@ -0,0 +1,6 @@
+// We can declare objects in two ways : 1- Literal and 2. Constructor
+
+//Singeltone : It is own type of object
+//object.create : This is cpnstructor method for singleTone
+
+// When we declare objects in literal then singleTone never happen 
