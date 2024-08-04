a9737d2297e2960c34fc2536a2e04d1454c24990
Nachiket Patel <142562120+Damn-Nachiket@users.noreply.github.com>
2024-08-04 15:15:55 +0000

    Array Notes Updated

--- BASIC2_JS/ARRAY.js
+++ BASIC2_JS/ARRAY.js
@@ -27,3 +27,39 @@ console.log( " B " , myArr)
 const myN2 = myArr.splice( 1, 3) // It Is Manipulative it can remove all entered range value
 console.log(myN2)
 console.log( " C ", myArr)
+
+// web console:
+
+// const myArr = [1,2,3,4]
+// myArr
+
+const heros = ["Walter White", "Jessy Pinkman" , "Montana"]
+const villians = [ "Tuco", "Dahmer" , "Pablo"]
+
+heros.push(villians)
+console.log(heros) // It will takes array as an element inside another value
+
+const together = heros.concat(villians)
+console.log(together)
+
+const all_new = [...heros , ...villians] //sprade operator
+//This is sprade here this is not single array its elements are now become individual
+console.log(all_new)
+
+const another_array = [1,2,3,[4,5,6],7,[8,9,[10]]]
+const usable_array = another_array.flat(Infinity)
+console.log(usable_array)
+
+console.log(Array.isArray("Nachi")) // It will check value whether it is present in array
+console.log(Array.from("Nachi")) // It will convert string to an array
+
+console.log(Array.from({name: "Nachi"}))
+// Interesting Situation ; it will return empty [] array
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3))
+// Read more about Array from , is and of.
+
