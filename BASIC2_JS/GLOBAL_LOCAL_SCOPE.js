1a47723d1763c4c37949fe85f2ec2c2775dfb7f6
Nachiket Patel <142562120+Damn-Nachiket@users.noreply.github.com>
2024-08-05 05:56:08 +0000

    Global and local scope added and noted some more advance coming

---
+++ BASIC2_JS/GLOBAL_LOCAL_SCOPE.js
@@ -0,0 +1,16 @@
+let a = 200
+const b = 150
+var c = 300
+
+if(true){
+    let a = 10
+    const b = 20
+    var c = 30
+    console.log("Inner Value of A : ",a)
+}
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+// Global Scope is different in both windows and code environmnet
