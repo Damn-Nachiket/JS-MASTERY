bd6dde2d845ebb183b98314677cb7c3abbc81ca7
Nachiket Patel <142562120+Damn-Nachiket@users.noreply.github.com>
2024-08-14 16:24:51 +0000

    While and do while loop example also mentioned

---
+++ ITERATIONS/WHILE_DO_WHILE.js
@@ -0,0 +1,22 @@
+// while (condition){}
+
+let index = 1
+while (index <= 10){
+    console.log(` Iteration of Index is ${index}`)
+    index = index + 2
+}
+
+let myArr = ['Gotham' , 'Batman' , 'Joker']
+let arr = 0
+while (arr < myArr.length){
+    console.log(`Value is ${myArr[arr]}`)
+    arr = arr + 1
+}
+
+// do {} while(condition)
+
+let score = 1
+do {
+    console.log(`Your score is ${score}`)
+    score++
+}while(score <= 5)
