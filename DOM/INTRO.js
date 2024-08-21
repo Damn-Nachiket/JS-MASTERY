62862c7560ce05e04d23be9b2be0edcd08325146
Nachiket Patel <142562120+Damn-Nachiket@users.noreply.github.com>
2024-08-21 14:43:43 +0000

    DOM basic and introduction done.

---
+++ DOM/INTRO.js
@@ -0,0 +1,12 @@
+// DOM - Documantation Object Model
+// Brenden Eich
+
+console.log(window)
+console.log(document)
+console.dir(document)
+
+console.log(docuemnt.links[2])
+document.getElementById('firstheading')
+
+// Do this practice in Wikipedia page of Brenden Eich
+document.getElementById('firstheading').innerHTML = "<h1> Nachiket Patel </h1>" // This is DOM manipulation
